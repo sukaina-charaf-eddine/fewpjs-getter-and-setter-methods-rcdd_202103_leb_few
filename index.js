4a525baa12b0f957cf9605a14c104758bb8fb155
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-05-09 12:32:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your Circle class here
+const pi = Math.PI
+
+class Circle {
+  constructor( radius ) {
+    this.radius = radius;
+  }
+diameter(){
+  return this.radius *2;
+}
+circumference(){
+  return (this.radius * 2) * pi;
+}
+area(){
+  return pi * (this.radius * this.radius);
+}  
+}
+
