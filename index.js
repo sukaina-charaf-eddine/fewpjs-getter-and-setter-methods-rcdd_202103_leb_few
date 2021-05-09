bb3190526b82383e1c8a2b83e6348cfbd7e87bb6
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-05-09 12:33:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ class Circle {
   constructor( radius ) {
     this.radius = radius;
   }
-diameter(){
-  return this.radius *2;
-}
-circumference(){
-  return (this.radius * 2) * pi;
-}
-area(){
-  return pi * (this.radius * this.radius);
-}
+
+  get diameter() {
+    return this.radius * 2
+  }
+
+  set diameter( diameter ) {
+    this.radius = diameter / 2
+  }
+
+  get circumference() {
+    return ( this.radius * 2 ) * pi
+  }
+
+  set circumference( circumference ) {
+    this.radius = circumference / ( pi * 2 )
+  }
+
+  get area() {
+    return pi * ( this.radius * this.radius )
+  }
+
+  set area( area ) {
+    this.radius = Math.sqrt( area / pi )
+  }
 }
